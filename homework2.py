5a58b1d6aff3a3715b0970f30069af18ff66a887
raymanan11 <47703160+raymanan11@users.noreply.github.com>
2019-03-17 01:36:22 -0700

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -48,6 +48,8 @@ def is_palindrome(phrase):
     return True
 
 def menu_check_palindrome():
+# the function menu_check_palindrome combines the function get_phrase() and is_palindrome(phrase) so that this function will get a phrase from get_phrase() and save the prhase into into is_palindrome(phrase)
+# if palindrome, which is a variable which equals and calls the function is_palindrome(phrase), is True, that means it is a palindrome and if false then is not a palindrome
     phrase = get_phrase()
     palindrome = is_palindrome(phrase)
     if palindrome == True:
@@ -56,23 +58,44 @@ def menu_check_palindrome():
         print(phrase, 'is not a palindrome!')
 
 def get_crossword_square():
-    st = input('Enter first string: ')
+    st = input('Please enter the first word of the square: ')
     order = len(st)
     for i in range(0, order-1):
-        st = st + input('Enter next string: ')
+    # for loop is from 0 to order-1 because you want the user to enter the next couple words relative to the order of the first word which is the length of the first word
+    # since the user already entered the first string the for loop it goes from 0 to order-1
+    # for example a 4 letter word, you would want to ask the user to enter 4 words to make a square
+    # since user already entered the first word, user should only be asked 3 more times to complete the square, which means with a for loop from range(0, order-1)
+    # with order being = to length of the first word which is four letters, this means order is 4. This means user will be asked three more times from range(0, order-1) = (0, 4-1) = range(0, 3) in this example
+    # meaning it will repeat three times to ask the user to input a word
+        st = st + input('Please enter the next word of the square: ')
+        # after user enters all the words, it will add the words together because strings can be concatenated and saved in one long string to be saved into one variable called st
+    st = st.lower()
+    # the function .lower() will lowercase all letters of the words that the user enters and will become a concatenated string and returned
+    # this is done so that it doens't matter the format the user types in whether it's capital and lowercase, it will be all lowercase so that it can work with the EnglishDictionary function in the next function
     return st
 
 def check_crossword_square(square):
     order = int(math.sqrt(len(square)))
+    # because the square will be the concatenated string, the order will be the square root of the string depending on how many characters are in the string
     for i in range(0, len(square), order):
         horizontal_words = square[i:i+order]
+        # for loop is written so that the i character will be determined by the order, or square root of the concatenated string
+        # to get the horizontal words, we would go from i to i+order
+        # an example to explain why I did this would be a string that has 16 characters, meaning the order is 4 because square root of 16 is 4. in order to get the horizontal words i would skip from 0 to 16 and land on every 4 characters
+        # because the horizontal words is [i:i+order], in this example the first i would be 0 and i+order would be 0+4 and so the first word is from [0:4] which would be correct because it gives us first 4 characters, and so on and so on
+        # this would continue as the for loop will give more i characters until it reaches end of the concatenated string
         check_horizontal_words = EnglishDictionary.is_word(horizontal_words)
+        # after getting horizontal words, EnglishDictionary.isword() function will check if the words are even words to begin with
     for i in range(0, order):
         vertical_words = square[i::order]
+        # there was a pattern to find the vertical words where if we started at one character and skipped to the end of the string using the stride, it would give us the vertical word
+        # so thats why range(0, order) was used because starting at position 0 and skipping to the next character and the next until the end of the string using the stride, would give us first vertical word and so on in the for loop
         check_vertical_words = EnglishDictionary.is_word(vertical_words)
+        # when vertical words are gotten, it will determine if vertical words are all words using EnglishDictionary.is_word() function
     if check_horizontal_words == True and check_vertical_words == True:
         return True
     return False
+    # if horizontal words are all words and vertical words are all words then it would be a crossword square which will return True and will return False if either horizontal words is not a word or vertical word is not a word
 
 def menu_check_crossword_square():
     crossword_string = get_crossword_square()
